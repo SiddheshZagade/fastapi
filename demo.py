1a3149bf4f86d1e0216c875c283d41e17f7e41d5
Siddhesh Zagade <siddeshzagade7@gmail.com>
2022-08-07 20:21:19 +0530

    new

--- demo.py
+++ demo.py
@@ -9,6 +9,12 @@ class Item(BaseModel):
     price: float
     brand: Optional[str]= None
 
+class UpdateItem(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    brand: Optional[str]= None
+
+
 @app.get("/")
 def home():
     return{"Data":"Testing"} 
@@ -18,12 +24,8 @@ def about():
     return {"Data":"About"} 
 
 inventory = {
-    1:{
-        "name":"Milk",
-        "price":3.99,
-        "brand":"Regular"
     }
-}    
+    
 
 @app.get("/get-item/{item_id}")
 def get_item(item_id:int=Path(None,description="The ID of the item you'd like to view")):
@@ -34,13 +36,19 @@ def create_item(item_id:int,item:Item):
     if item_id in inventory: 
      return{"Error":"Item ID already exists"}
 
-    inventory[item_id]={"name":item.name, "brand": item.brand,"price":item.price}
+    inventory[item_id]=item
     return inventory[item_id]
 
 @app.put("/update-item/{item_id}")
-def update_item(item_id:int,item:Item):
+def update_item(item_id: int,item:UpdateItem):
     if item_id not in inventory: 
      return{"Error":"Item ID does not exists"}
-
-    inventory[item_id].update={"name":item.name, "brand": item.brand,"price":item.price}
+ 
+    if item.name != None:
+        inventory[item_id].name = item.name 
+    if item.price != None:
+        inventory[item_id].price = item.price 
+    if item.brand != None:
+        inventory[item_id].brand = item.brand 
+    
     return inventory[item_id]
